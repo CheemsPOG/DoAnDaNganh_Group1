74699223f8e0a446dc81b37140928122e9cc4b21
minhhieutaro <minhhieutaro@gmail.com>
2025-04-02 22:50:49 +0700

    Fix Frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -9,18 +9,14 @@ export default function Home() {
       <h1 className="text-4xl font-bold mb-8">Home Page</h1>
       <div className="space-y-4">
         <Link href="/temp">
-          <a className="block">
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
               Go to Temp Page
             </button>
-          </a>
         </Link>
         <Link href="/light">
-          <a className="block">
             <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded">
               Go to Light Page
             </button>
-          </a>
         </Link>
       </div>
     </div>
